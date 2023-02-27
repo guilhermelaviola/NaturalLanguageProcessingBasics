b78430c541e8ad8e1f54e76471a65fdf4b270b85
guilherme <guilhermelaviola96@gmail.com>
2023-02-27 19:23:35 -0300

    Added an example of Lemmatization. There is an error which I'm working on.

---
+++ techniques/lemmatization.py
@@ -0,0 +1,21 @@
+# Tokenization and Lemmatization example with SpaCy
+import spacy
+
+nlp = spacy.load('en_core_web_sm')
+
+# Text example to be used
+doc = nlp('The quick brown fox \t jumps over the lazy dog.')
+
+# Displaying the tokens and whether they are spaces,
+# punctuations, uppercase, digit or stopwords or not
+for token in doc:
+    print(token, token.is_space, token.is_punct, token.is_upper, token.is_digit, token.is_stop)
+
+# Manipulating the Lemmatization
+for token in doc:
+    print(f'(token.text) \t (token.lemma_)')
+
+# Part-of-the-speech tagging. In other words, it checks whether each of
+# the tokenized words are verbs, nouns, adverbs, adjectives, etc...
+for token in doc:
+    print(f'(token.text) \t (token.pos_)')
